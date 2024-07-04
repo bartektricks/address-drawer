0aed2c72f70d5a6c45e38da3e5f25999a3431b5f
BartekTricks <bartektricks@gmail.com>
2024-07-04 22:45:01 +0200

    chore: clean up shared props logic and improve linting

--- src/components/address-form/AddressForm.tsx
+++ src/components/address-form/AddressForm.tsx
@@ -1,12 +1,10 @@
 import { Formik } from "formik";
-import { LuMapPin } from "react-icons/lu";
 import AddressSearch from "../address-search/AddressSearch";
 import Button from "../button/Button";
-import Combobox from "../form/Combobox";
 import MultiSelect from "../form/MultiSelect";
 import TextArea from "../form/TextArea";
 import { departments } from "./helpers";
-import { AddressFormSchema } from "./schema";
+import { AddressFormSchema, type FieldKeys } from "./schema";
 
 export default function AddressForm() {
 	return (
@@ -26,40 +24,47 @@ export default function AddressForm() {
 				values,
 				errors,
 				touched,
-			}) => (
-				<form onSubmit={handleSubmit} className="flex flex-col gap-4">
-					<AddressSearch
-						iconStart={LuMapPin}
-						title="Search for an address"
-						placeholder="Type your address here..."
-						name="address"
-						value={values.address}
-						onSelect={handleChange}
-						error={touched.address ? errors.address : undefined}
-					/>
-					<MultiSelect
-						title="Attach the address above to one or more departments"
-						placeholder="Select department(s)"
-						name="departments"
-						value={values.departments}
-						onChange={handleChange}
-						fields={departments}
-						error={touched.departments ? errors.departments : undefined}
-					/>
-					<TextArea
-						title="Address description (optional)"
-						placeholder="Write here..."
-						name="description"
-						onChange={handleChange}
-						onBlur={handleBlur}
-						error={touched.description ? errors.description : undefined}
-						value={values.description}
-					/>
-					<Button className="self-end" type="submit">
-						Add address
-					</Button>
-				</form>
-			)}
+			}) => {
+				const getErrorMessage = (name: FieldKeys) => {
+					const error = errors[name];
+					const message = Array.isArray(error) ? error.join(", ") : error;
+
+					return touched[name] ? message : undefined;
+				};
+
+				const getSharedProps = <T extends FieldKeys>(name: T) => ({
+					name,
+					onChange: handleChange,
+					onSelect: handleChange,
+					onBlur: handleBlur,
+					value: values[name],
+					error: getErrorMessage(name),
+				});
+
+				return (
+					<form onSubmit={handleSubmit} className="flex flex-col gap-4">
+						<AddressSearch
+							title="Search for an address"
+							placeholder="Type your address here..."
+							{...getSharedProps("address")}
+						/>
+						<MultiSelect
+							title="Attach the address above to one or more departments"
+							placeholder="Select department(s)"
+							{...getSharedProps("departments")}
+							fields={departments}
+						/>
+						<TextArea
+							title="Address description (optional)"
+							placeholder="Write here..."
+							{...getSharedProps("description")}
+						/>
+						<Button className="self-end" type="submit">
+							Add address
+						</Button>
+					</form>
+				);
+			}}
 		</Formik>
 	);
 }
--- src/components/address-form/schema.ts
+++ src/components/address-form/schema.ts
@@ -2,10 +2,9 @@ import * as Yup from "yup";
 
 export const AddressFormSchema = Yup.object().shape({
 	address: Yup.string().required("Address is required."),
-	departments: Yup.array(Yup.string()).min(
-		1,
-		"Select at least one department.",
-	),
+	departments: Yup.array(Yup.string().required())
+		.min(1, "Select at least one department.")
+		.required(),
 	description: Yup.string().optional(),
 });
 
