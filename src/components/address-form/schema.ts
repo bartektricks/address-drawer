3898ec6fbd361ceaf351a63662fd168e879c9245
BartekTricks <bartektricks@gmail.com>
2024-07-05 11:09:54 +0200

    chore: update schema with defaults

--- src/components/address-form/schema.ts
+++ src/components/address-form/schema.ts
@@ -4,8 +4,9 @@ export const AddressFormSchema = Yup.object({
 	address: Yup.string().required("Address is required."),
 	departments: Yup.array(Yup.string().required())
 		.min(1, "Select at least one department.")
-		.required(),
-	description: Yup.string().optional(),
+		.required()
+		.default([]),
+	description: Yup.string().optional().default(""),
 });
 
 export type Fields = Yup.InferType<typeof AddressFormSchema>;
