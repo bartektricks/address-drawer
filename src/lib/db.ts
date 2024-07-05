9470be4076160643fd895a070e6a1d1ec3941216
BartekTricks <bartektricks@gmail.com>
2024-07-05 11:09:38 +0200

    feat: create mock db

---
+++ src/lib/db.ts
@@ -0,0 +1,42 @@
+export const DB_KEY = "db";
+
+export type Address = {
+	id: string;
+	address: string;
+	departments: string[];
+	description: string;
+};
+
+type DbData = {
+	addresses: Address[];
+};
+
+const db = {
+	get(): DbData | null {
+		const db = localStorage.getItem(DB_KEY);
+		return db ? JSON.parse(db) : null;
+	},
+	set(address: Address) {
+		const db = this.get();
+
+		localStorage.setItem(
+			DB_KEY,
+			JSON.stringify({
+				addresses: [...(!db ? [address] : [...db.addresses, address])],
+			}),
+		);
+	},
+	remove(id: string) {
+		const db = this.get();
+
+		if (!db) return;
+
+		const addresses = db.addresses.filter((address) => address.id !== id);
+		localStorage.setItem(DB_KEY, JSON.stringify({ addresses }));
+	},
+	clearAll() {
+		localStorage.removeItem(DB_KEY);
+	},
+};
+
+export default db;
